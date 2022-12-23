180c3296ecc2bad7f08ebdbb79c6d2084d7167c0
Prashant <prashant.chaurasiya.cp@gmail.com>
2022-12-23 18:27:59 +0530

    [Prashant]: Handle Exception

---
+++ HandleException/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace HandleException
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message1 = "I am in Sad mood";
+            Console.WriteLine(message1);
+            MoodAnalyser mood1 = new MoodAnalyser(message1);
+            Console.WriteLine(mood1.Analyser());
+
+            string message2 = "I am in any mood";
+            Console.WriteLine(message2);
+            MoodAnalyser mood2 = new MoodAnalyser(message2);
+            Console.WriteLine(mood2.Analyser());
+        }
+    }
+}
---
+++ HandlingTestProject/UnitTest1.cs
@@ -0,0 +1,47 @@
+using HandleException;
+
+namespace HandlingTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [TestCategory("Happy Mood")]
+        public void TestMethodAnalyserForHappyMood()
+        {
+            string message = "Im in happy mood";
+            string expected = "HAPPY";
+            MoodAnalyser moodAnalyser = new MoodAnalyser(message);
+
+            string Actual = moodAnalyser.Analyser();
+
+            Assert.AreEqual(expected, Actual);
+        }
+        [TestMethod]
+        [TestCategory("Sad Mood")]
+        public void TestMethodAnalayserForSadMood()
+        {
+            string message = "Im in sad mood";
+            string expected = "SAD";
+            MoodAnalyser moodAnalyser = new MoodAnalyser(message);
+
+            string Actual = moodAnalyser.Analyser();
+
+            Assert.AreEqual(expected, Actual);
+        }
+        [TestMethod]
+        [TestCategory("Null Case")]
+        public void GivenNullReturnHappyMood()
+        {
+            string message = "null";
+            string expected = "HAPPY";
+            MoodAnalyser moodAnalyser = new MoodAnalyser(message);
+
+            string Actual = moodAnalyser.Analyser();
+
+            Assert.AreEqual(expected, Actual);
+        }
+    }
+
+}
+    
